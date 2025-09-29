ccfbae1a85e60c9221603229a04857bbf2c1586c
drowecode <drowe7@lion.lmu.edu>
2025-09-29 13:21:49 -0700

    changed to js

---
+++ App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Footer from "./Footer";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Gas Prices In</h1>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
+}

76656df1ac0b40aec531e4cce5f492587232c2f2
Christian Love <clove10200@gmail.com>
2025-10-07 16:34:14 -0700

    added error checking, finished table header backend

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,50 @@ import "./App.css";
 import intelLogo from '/src/assets/AllianceIntelligence.svg'
 import jediLogo from '/src/assets/Rebel_Alliance_Logo.svg'
 
-function setColumns(dropdown) {
-  if (dropdown == "starships") {
-    columnTwo = "Manufacturer";
-    columnThree = "Model";
-  } else if (dropdown == "people") {
-    columnTwo = "Height(cm)";
-    columnThree = "Birth Year";
-  } else if (dropdown == "planets") {
-    columnTwo = "Terrain";
-    columnThree = "Population";
-  }
-}
+
 
 export default function App() {
   const [results, setResults] = useState([]);
   const [dropdown, setDropdown] = useState("");
   const [pagedropdown, setPageDropdown] = useState("");
   const [credentials, credentialsVerify] = useState(false);
-  const columnOne = "";
-  const columnTwo = "";
-  const columnThree = "";
+  const [columnOne, setColumnOne] = useState("");
+  const [columnTwo, setColumnTwo] = useState("");
+  const [columnThree, setColumnThree] = useState("");
 
+  function doubleFunction(dropdown){
+  setDropdown(dropdown),
+  setColumn(dropdown);
+}
+function setColumn(dropdown) {
+  if (dropdown === "starships") {
+    setColumnOne("Name");
+    setColumnTwo("Manufacturer");
+    setColumnThree("Model");
+  } else if (dropdown === "people") {
+    setColumnOne("Name");
+    setColumnTwo("Height (cm)");
+    setColumnThree("Birthdate");
+  } else if (dropdown === "planets") {
+    setColumnOne("Name");
+    setColumnTwo("Terrain");
+    setColumnThree("Population");
+  }
+}
   const url = "https://swapi.dev/api/";
   useEffect(() => {
     fetch(url + dropdown + pagedropdown)
       .then((r) => r.json())
       .then((response) => {
         setResults(response.results);
-      });
+      })
+      .catch(error =>{console.log('Network error. Please try again.', error)});
   });
 
   return (
     <div className="App">
       <h1>Rebel Alliance Intelligence Database</h1>
-      <div class="container">
+      <div className="container">
         <div><img src={intelLogo} alt="intel logo" width="300" height="200"></img></div>
         <div></div>
         <div><img src={jediLogo} alt="jedi logo" width="300" height="200"></img></div>
@@ -49,26 +58,32 @@ export default function App() {
       ) : (
         <button>Credentials verified. Welcome, commander.</button>
       )}
-      <table>
-        {credentials ? (
-          <tbody>
-            <select
+    {credentials ? (
+      <div className="container">
+        <div><select
               value={dropdown}
-              onChange={(x) => setDropdown(x.target.value)}
+              onChange={(x) => doubleFunction(x.target.value)}
             >
               <option value="">Choose a database to query.</option>
-              <option value="starships">Starships</option>
-              <option value="people">People</option>
-              <option value="planets">Planets</option>
+              <option value="starships">Starship Registry</option>
+              <option value="people">Summary Dossiers</option>
+              <option value="planets">Planetary Database</option>
             </select>
-            <select
+        </div>
+        <div><select
               value={pagedropdown}
               onChange={(y) => setPageDropdown(y.target.value)}
             >
               <option value="/?page=1">1</option>
               <option value="/?page=2">2</option>
               <option value="/?page=3">3</option>
-            </select>
+            </select></div>
+      </div>
+    ) : (<div></div>
+    )}
+      <table>
+        {credentials ? (
+          <tbody>
             <tr>
               <th>{columnOne}</th>
               <th>{columnTwo}</th>
